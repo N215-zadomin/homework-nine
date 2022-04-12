a5a53550386a17bf0e11d388969971887c2eb363
zaydee8223 <83433819+zaydee8223@users.noreply.github.com>
2022-04-11 22:50:03 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Homework Nine
+
+Web4 Link:
+https://in-info-web4.informatics.iupui.edu/~zadomin/homework-nine/
+
+This week, our assignment was pretty much the same as homeowork eight's. We were to add content to the page when you click on a link by using the MVC model and jQuery. We were to again only use one page and load all the content onto that specific page. It needed to be fully developed and look like an actual website. I feel like the only struggle I really had this week was understanding the different uses of app.js and model.js. Other than thatm this assignment was not that bad to complete.
---
+++ model/model.js
@@ -0,0 +1,297 @@
+// this info simulates getting data from a database
+var homeContent = `<div class="hero"></div>
+<div class="content">
+  <h1>Rolling in the Deep</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image1"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          There's a fire starting in my heart Reaching a fever pitch and
+          it's bringing me out the dark Finally, I can see you crystal
+          clear Go ahead and sell me out and I'll lay your ship bare See
+          how I'll leave with every piece of you Don't underestimate the
+          things that I will do There's a fire starting in my heart
+          Reaching a fever pitch and it's bring me out the dark The
+          scars of your love remind me of us They keep me thinking that
+          we almost had it all The scars of your love, they leave me
+          breathless I can't help feeling We could have had it all
+          (You're gonna wish you never had met me) Rolling in the deep
+          (Tears are gonna fall, rolling in the deep) You had my heart
+          inside of your hands (You're gonna wish you never had met me)
+          And you played it to the beat (Tears are gonna fall, rolling
+          in the deep)
+        </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          Baby, I have no story to be told But I've heard one on you and
+          I'm gonna make your head burn Think of me in the depths of
+          your despair Make a home down there as mine sure won't be
+          shared The scars of your love remind me of us (Tears are gonna
+          fall, rolling in the deep) They keep me thinking that we
+          almost had it all (You're gonna wish you never had met me) The
+          scars of your love, they leave me breathless (Tears are gonna
+          fall, rolling in the deep) I can't help feeling We could have
+          had it all (You're gonna wish you never had met me) Rolling in
+          the deep (Tears are gonna fall, rolling in the deep) You had
+          my heart inside of your hands (You're gonna wish you never had
+          met me) And you played it to the beat (Tears are gonna fall,
+          rolling in the deep) Could have had it all Rolling in the deep
+          You had my heart inside of your hands But you played it with a
+          beating
+        </p>
+      </div>
+      <div class="prod-image2"></div>
+    </div>
+
+    <div class="product">
+      <iframe
+        width="350"
+        height="350"
+        src="https://www.youtube.com/embed/rYEDA3JcQqw"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div class="prod-descrip">
+        <p>
+          Throw your soul through every open door Count your blessings
+          to find what you look for Turn my sorrow into treasured gold
+          You'll pay me back in kind and reap just what you've sown We
+          could have had it all (Tears are gonna fall, rolling in the
+          deep) We could have had it all (You're gonna wish you never
+          had met me) It all, it all, it all (Tears are gonna fall,
+          rolling in the deep) We could have had it all (You're gonna
+          wish you never had met me) Rolling in the deep (Tears are
+          gonna fall, rolling in the deep) You had my heart inside of
+          your hands (You're gonna wish you never had met me) And you
+          played it to the beat (Tears are gonna fall, rolling in the
+          deep) We could have had it all (You're gonna wish you never
+          had met me) Rolling in the deep (Tears are gonna fall, rolling
+          in the deep) You had my heart inside of your hands (You're
+          gonna wish you never had met me) But you played it You played
+          it You played it You played it to the beat
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+var aboutContent = ` <div class="hero"></div>
+<div class="content">
+  <h1>Rumour Has It</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image3"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          She, she ain't real She ain't gon' be able to love you like I
+          will She is a stranger You and I have history Or don't you
+          remember Sure, she's got it all But, baby, is that really what
+          you want Bless your soul, you got your head in the clouds She
+          made a fool out of you And, boy, she's bringing you down She
+          made your heart melt But you're cold to the core Now rumor has
+          it she ain't got your love anymore Rumor has it (rumor) Rumor
+          has it (rumor) Rumor has it (rumor) Rumor has it (rumor) Rumor
+          has it (rumor) Rumor has it (rumor) Rumor has it (rumor) Rumor
+          has it (rumor)
+        </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          She is half your age But I'm guessing that's the reason that
+          you stayed I heard you've been missing me You've been telling
+          people things you shouldn't be Like when we creep out and she
+          ain't around Haven't you heard the rumors Bless your soul, you
+          got your head in the clouds You made a fool out of me And,
+          boy, you're bringing me down You made my heart melt, yet I'm
+          cold to the core But rumor has it I'm the one you're leaving
+          her for Rumor has it (rumor) Rumor has it (rumor) Rumor has it
+          (rumor) Rumor has it (rumor) Rumor has it (rumor) Rumor has it
+          (rumor) Rumor has it (rumor) Rumor has it (rumor)
+        </p>
+      </div>
+      <div class="prod-image4"></div>
+    </div>
+
+    <div class="product">
+      <iframe
+        width="350"
+        height="350"
+        src="https://www.youtube.com/embed/HF-LllSQza0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div class="prod-descrip">
+        <p>
+          All of these words whispered in my ear Tell a story that I
+          cannot bear to hear Just 'cause I said it, it don't mean that
+          I meant it People say crazy things Just 'cause I said it,
+          don't mean that I meant it Just 'cause you heard it Rumor has
+          it (rumor) Rumor has it (rumor) Rumor has it (rumor) Rumor has
+          it (rumor) Rumor has it (rumor) Rumor has it (rumor) Rumor has
+          it (rumor) Rumor has it (rumor-rumor) Rumor has it (rumor)
+          Rumor has it (rumor) Rumor has it (rumor) Rumor has it (rumor)
+          Rumor has it (rumor) Rumor has it But rumor has it he's the
+          one I'm leaving you for
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+var productsContent = `<div class="hero"></div>
+<div class="content">
+  <h1>Set Fire to the Rain</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image5"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          I let it fall, my hart And as it fell, you rose to claim it It
+          was dark, and I was over Until you kissed my lips and you
+          saved me My hands, they were strong But my knees were far too
+          weak To stand in your arms Without falling to your feet But
+          there's a side to you That I never knew, never knew All the
+          things you'd say They were never true, never true And the
+          games you'd play You would always win, always win But I set
+          fire to the rain Watched it pour as I touched your face Well,
+          it burned while I cried 'Cause I heard it screaming out your
+          name, your name! When I lay with you I could stay there Close
+          my eyes Feel you here forever You and me together Nothing is
+          better
+        </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          'Cause there's a side to you That I never knew, never knew All
+          the things you'd say They were never true, never true And the
+          games you'd play You would always win, always win But I set
+          fire to the rain Watched it pour as I touched your face Well,
+          it burned while I cried 'Cause I heard it screaming out your
+          name, your name! I set fire to the rain And I threw us into
+          the flames When it fell, something died 'Cause I knew that
+          that was the last time, the last time! Sometimes I wake up by
+          the door That heart you caught must be waiting for you
+        </p>
+      </div>
+      <div class="prod-image6"></div>
+    </div>
+
+    <div class="product">
+      <iframe
+        width="350"
+        height="350"
+        src="https://www.youtube.com/embed/Ri7-vnrJD3k"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div class="prod-descrip">
+        <p>
+          Even now, when we're already over I can't help myself from
+          looking for you I set fire to the rain Watched it pour as I
+          touched your face Well, it burned while I cried 'Cause I heard
+          it screaming out your name, your name I set fire to the rain
+          And I threw us into the flames When it fell, something died
+          'Cause I knew that that was the last time, the last time, oh!
+          Oh, no Let it burn, oh Let it burn Let it burn
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+var contactContent = `<div class="hero"></div>
+<div class="content">
+  <h1>Someone Like You</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image7"></div>
+      <div class="prod-descrip">
+        <h4>Lyrics:</h4>
+        <p>
+          I heard that you're settled down That you found a girl and
+          you're married now I heard that your dreams came true Guess
+          she gave you things, I didn't give to you Old friend, why are
+          you so shy? Ain't like you to hold back or hide from the light
+          I hate to turn up out of the blue, uninvited But I couldn't
+          stay away, I couldn't fight it I had hoped you'd see my face
+          And that you'd be reminded that for me, it isn't over Never
+          mind, I'll find someone like you I wish nothing but the best
+          for you, too "Don't forget me, " I beg I remember you said
+        </p>
+      </div>
+    </div>
+
+    <div class="product">
+      <div class="prod-descrip">
+        <p>
+          "Sometimes it lasts in love, but sometimes it hurts instead"
+          "Sometimes it lasts in love, but sometimes it hurts instead"
+          You know how the time flies Only yesterday was the time of our
+          lives We were born and raised in a summer haze Bound by the
+          surprise of our glory days I hate to turn up out of the blue,
+          uninvited But I couldn't stay away, I couldn't fight it I had
+          hoped you'd see my face And that you'd be reminded that for
+          me, it isn't over Never mind, I'll find someone like you I
+          wish nothing but the best for you, too "Don't forget me, " I
+          begged I remember you said "Sometimes it lasts in love, but
+          sometimes it hurts instead" Nothing compares, no worries or
+          cares Regrets and mistakes, they're memories made
+        </p>
+      </div>
+      <div class="prod-image8"></div>
+    </div>
+
+    <div class="product">
+      <iframe
+        width="350"
+        height="350"
+        src="https://www.youtube.com/embed/hLQl3WQQoQ0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div class="prod-descrip">
+        <p>
+          Who would have known how bittersweet this would taste? Never
+          mind, I'll find someone like you I wish nothing but the best
+          for you "Don't forget me, " I beg I remember you said
+          "Sometimes it lasts in love, but sometimes it hurts instead"
+          Never mind, I'll find someone like you I wish nothing but the
+          best for you, too "Don't forget me, " I begged I remember you
+          said "Sometimes it lasts in love, but sometimes it hurts
+          instead" "Sometimes it lasts in love, but sometimes it hurts
+          instead"
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+// this function is being called from the app.js and it has the page name
+export function modelPageName(pageName) {
+    console.log(pageName);
+    $("#app").html(eval(pageName));
+}
+
